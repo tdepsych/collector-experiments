2d67e5acde52005da6f4ddd1cb361a1ba5e5286d
CG Dobson <chrisguydobson@gmail.com>
2022-03-25 09:19:44 +0000

    fixed origin elements firefox

--- User/Assets/MSc/zoom_script.js
+++ User/Assets/MSc/zoom_script.js
@@ -18,12 +18,12 @@ function zoom_in() {
       (window.innerWidth * 0.97) / parent.parent.current_zoom;
       this_iframe_style.maxHeight =
       (window.innerHeight * 0.97)  / parent.parent.current_zoom;
-      this_iframe_style.transformOrigin = "center top";
+      this_iframe_style.transformOrigin = "500px 0px";
     } else {
       this_iframe_style.width = "100%";
       this_iframe_style.height = "100%";
     }
-  document.body.style.transformOrigin = "center top";
+  document.body.style.transformOrigin = "left top";
   document.body.style.overflowX = "hidden";
 }
 function zoom_out() {
@@ -40,7 +40,7 @@ function zoom_out() {
       (window.innerWidth * 0.97) / parent.parent.current_zoom;
       this_iframe_style.maxHeight =
       (window.innerHeight * 0.97)  / parent.parent.current_zoom;
-      this_iframe_style.transformOrigin = "center top";
+      this_iframe_style.transformOrigin = "50% 0%";
     } else {
       this_iframe_style.width = "100%";
       this_iframe_style.height = "100%";
