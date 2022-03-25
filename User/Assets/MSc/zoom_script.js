8a805a09ececa4cb3932a2d71f1f5f61e8087a34
CG Dobson <chrisguydobson@gmail.com>
2022-03-25 10:28:17 +0000

    focus on zoomOut

--- User/Assets/MSc/zoom_script.js
+++ User/Assets/MSc/zoom_script.js
@@ -53,7 +53,4 @@ function zoom_out() {
 document.getElementById("zoomIn").onclick = zoom_in;
 document.getElementById("zoomOut").onclick = zoom_out;
 
-var elelist = document.getElementsByTagName("button");
-for(var i = 0; i < elelist.length; i++){
-    elelist[i].blur();
-}
+document.getElementById('zoomOut').focus();
