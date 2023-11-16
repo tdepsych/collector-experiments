266ba3c4ef6734a6c9e6e42e1d885586807f4955
CG Dobson <chrisguydobson@gmail.com>
2023-11-16 17:31:00 +0000

    I hate FF scaling now!

--- User/Assets/Global/js/zoom_script.js
+++ User/Assets/Global/js/zoom_script.js
@@ -23,8 +23,8 @@ function zoom_out() {
   document.body.style.overflowX = "hidden";
 }
 
-// window.onload = function(){ 
-window.onpageshow = function(){ 
+window.onload = function(){ 
+// window.onpageshow = function(){ 
   $("#zoomIn").click(zoom_in);
   $("#zoomOut").click(zoom_out);
 }
