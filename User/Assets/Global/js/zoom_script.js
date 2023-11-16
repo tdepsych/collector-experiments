3f8928029179b5dca01dab9615df6506f6d6675d
CG Dobson <chrisguydobson@gmail.com>
2023-11-16 15:52:44 +0000

    Fix for FF scaling elements off screen

--- User/Assets/Global/js/zoom_script.js
+++ User/Assets/Global/js/zoom_script.js
@@ -4,12 +4,27 @@ if (parent.parent.current_zoom == null){
   parent.parent.current_zoom = parent.parent.current_zoom;
 }
 
+$.fn.isInViewport = function() {
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+   
 function zoom_in() {
-  parent.parent.current_zoom += 0.1;
+  if ($('#zoomOut').isInViewport()) {
+    parent.parent.current_zoom += 0.1;
+  } else {
+    // appropriate_message("This will zoom in beyond your screen size");
+    console.log("It'd break");
+    parent.parent.current_zoom = 1;
+  }
   document.body.style.zoom = parent.parent.current_zoom;
   document.body.style.MozTransform = "scale(" + parent.parent.current_zoom + ")";
   document.body.style.overflowX = "hidden";
   // document.body.style.transformOrigin = "center center";
+
 }
 function zoom_out() {
   parent.parent.current_zoom -= 0.1;
